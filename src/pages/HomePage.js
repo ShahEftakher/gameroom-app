5635276f2f9a839798f5cdf495a681602fc5c1bc
ShahEftakher <shaheftakher@iut-dhaka.edu>
2021-02-25 18:05:13 +0600

    Carousel added

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import CarouselComp from "../components/CarouselComp";
 
 const Homepage = () => {
   return (
     <div>
       <Navbar />
+      <CarouselComp />
       home page
     </div>
   );
