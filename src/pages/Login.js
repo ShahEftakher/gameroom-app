11e75e347d26b0510d956a08707fa7b217c598d0
ESD <ESD@DESKTOP-SNRCVRJ>
2021-02-25 09:50:46 +0600

    first commit

---
+++ src/pages/Login.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Login = () => {
+  return <div>:D</div>;
+};
+
+export default Login;
---
+++ src/pages/Signup.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Signup = () => {
+  return <div>
+  fuck my life
+  </div>;
+};
+
+export default Signup;
