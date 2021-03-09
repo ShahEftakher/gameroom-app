1e123d8a4f5458d00e861068dca2fc6357befee8
Saad-IUT <saadbinjohir@gmail.com>
2021-03-10 02:03:31 +0600

    Video title

--- src/components/video/Videoplayer.js
+++ src/components/video/Videoplayer.js
@@ -1,120 +1,120 @@
-import React, { useState, useRef } from 'react';
-import ReactPlayer from 'react-player';
-import Controls from './Videocontrol';
-import { makeStyles } from '@material-ui/core/styles';
-import screenful from 'screenfull';
+import React, { useState, useRef } from 'react'
+import ReactPlayer from 'react-player'
+import Controls from './Videocontrol'
+import { makeStyles } from '@material-ui/core/styles'
+import screenful from 'screenfull'
 const useStyles = makeStyles({
   playerWrapper: {
     height: '90vh',
     position: 'relative',
   },
-});
+})
 
-const format = (seconds) => {
+const format = seconds => {
   if (isNaN(seconds)) {
-    return `00:00`;
+    return `00:00`
   }
-  const date = new Date(seconds * 1000);
-  const hh = date.getUTCHours();
-  const mm = date.getUTCMinutes();
-  const ss = date.getUTCSeconds().toString().padStart(2, '0');
+  const date = new Date(seconds * 1000)
+  const hh = date.getUTCHours()
+  const mm = date.getUTCMinutes()
+  const ss = date.getUTCSeconds().toString().padStart(2, '0')
   if (hh) {
-    return `${hh}:${mm.toString().padStart(2, '0')}:${ss}`;
+    return `${hh}:${mm.toString().padStart(2, '0')}:${ss}`
   }
-  return `${mm}:${ss}`;
-};
+  return `${mm}:${ss}`
+}
 
-let count = 0;
-const videoId = window.location.pathname.split('/').pop();
+let count = 0
+const videoId = window.location.pathname.split('/').pop()
 
 function VideoPlayer(props) {
-  const classes = useStyles();
+  const classes = useStyles()
   const [state, setState] = useState({
     playing: true,
     muted: false,
     volume: 0.5,
     played: 0,
     seeking: false,
-  });
+  })
 
-  const { playing, muted, volume, played } = state;
+  const { playing, muted, volume, played } = state
 
-  const playerRef = useRef(null);
-  const playerContainerRef = useRef(null);
-  const controlsRef = useRef(null);
+  const playerRef = useRef(null)
+  const playerContainerRef = useRef(null)
+  const controlsRef = useRef(null)
 
   const handlePlayPause = () => {
-    setState({ ...state, playing: !state.playing });
-  };
+    setState({ ...state, playing: !state.playing })
+  }
 
   const handleRewind = () => {
-    playerRef.current.seekTo(playerRef.current.getCurrentTime() - 10);
-  };
+    playerRef.current.seekTo(playerRef.current.getCurrentTime() - 10)
+  }
 
   const handleForward = () => {
-    playerRef.current.seekTo(playerRef.current.getCurrentTime() + 10);
-  };
+    playerRef.current.seekTo(playerRef.current.getCurrentTime() + 10)
+  }
 
   const handleMute = () => {
-    setState({ ...state, muted: !state.muted });
-  };
+    setState({ ...state, muted: !state.muted })
+  }
 
   const handleVolumeSeekUp = (e, newValue) => {
-    setState({ ...state, seeking: false, volume: parseFloat(newValue / 100) });
-  };
+    setState({ ...state, seeking: false, volume: parseFloat(newValue / 100) })
+  }
 
   const handleVolumeChange = (e, newValue) => {
     setState({
       ...state,
       volume: parseFloat(newValue / 100),
       muted: newValue === 0 ? true : false,
-    });
-  };
+    })
+  }
 
   const handleFullScreen = () => {
-    screenful.toggle(playerContainerRef.current);
-  };
+    screenful.toggle(playerContainerRef.current)
+  }
 
-  const handleProgress = (changeState) => {
+  const handleProgress = changeState => {
     if (count > 3) {
-      controlsRef.current.style.visibility = 'hidden';
-      count = 0;
+      controlsRef.current.style.visibility = 'hidden'
+      count = 0
     }
     if (controlsRef.current.style.visibility === 'visible') {
-      count += 1;
+      count += 1
     }
     if (!state.seeking) {
-      setState({ ...state, ...changeState });
+      setState({ ...state, ...changeState })
     }
-  };
+  }
 
   const handleSeek = (e, newValue) => {
-    setState({ ...state, played: parseFloat(newValue / 100) });
-  };
+    setState({ ...state, played: parseFloat(newValue / 100) })
+  }
 
-  const handleSeekMouseDown = (e) => {
-    setState({ ...state, seeking: true });
-  };
+  const handleSeekMouseDown = e => {
+    setState({ ...state, seeking: true })
+  }
 
   const handleSeekMouseUp = (e, newValue) => {
-    setState({ ...state, seeking: false });
-    playerRef.current.seekTo(newValue / 100, 'fraction');
-  };
+    setState({ ...state, seeking: false })
+    playerRef.current.seekTo(newValue / 100, 'fraction')
+  }
 
   const currentTime =
     playerRef && playerRef.current
       ? playerRef.current.getCurrentTime()
-      : '00:00';
+      : '00:00'
 
   const duration =
-    playerRef && playerRef.current ? playerRef.current.getDuration() : '00:00';
+    playerRef && playerRef.current ? playerRef.current.getDuration() : '00:00'
 
-  const remainingTime = format(duration - currentTime);
+  const remainingTime = format(duration - currentTime)
 
   const handleMouseMove = () => {
-    controlsRef.current.style.visibility = 'visible';
-    count = 0;
-  };
+    controlsRef.current.style.visibility = 'visible'
+    count = 0
+  }
   return (
     <>
       <div
@@ -126,8 +126,8 @@ function VideoPlayer(props) {
           ref={playerRef}
           muted={muted}
           playing={playing}
-          height="100%"
-          width="100%"
+          height='100%'
+          width='100%'
           url={props.url}
           volume={volume}
           onProgress={handleProgress}
@@ -149,11 +149,11 @@ function VideoPlayer(props) {
           onSeekMouseDown={handleSeekMouseDown}
           onSeekMouseUp={handleSeekMouseUp}
           remainingTime={remainingTime}
-          //   title={title}
+          title={props.title}
         />
       </div>
     </>
-  );
+  )
 }
 
-export default VideoPlayer;
+export default VideoPlayer
--- src/pages/videopages/VideoPage.js
+++ src/pages/videopages/VideoPage.js
@@ -1,77 +1,77 @@
-import React, { useEffect, useState } from 'react';
-import Navbar from '../../components/common/Navbar';
-import VideoPlayer from '../../components/video/Videoplayer';
-import { db } from '../../firebase';
-import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
-import VideoComments from '../../components/video/VideoComments';
+import React, { useEffect, useState } from 'react'
+import Navbar from '../../components/common/Navbar'
+import VideoPlayer from '../../components/video/Videoplayer'
+import { db } from '../../firebase'
+import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder'
+import VideoComments from '../../components/video/VideoComments'
 
-const VideoPage = (props) => {
-  const videoId = window.location.pathname.split('/').pop();
-  const [videoData, setVideoData] = useState('');
-  const [likes, setLikes] = useState(0);
-  const [disabled, setDisable] = useState({});
-  const [views, setView] = useState(0);
+const VideoPage = props => {
+  const videoId = window.location.pathname.split('/').pop()
+  const [videoData, setVideoData] = useState('')
+  const [likes, setLikes] = useState(0)
+  const [disabled, setDisable] = useState({})
+  const [views, setView] = useState(0)
 
   const addLike = () => {
-    setLikes(likes + 1);
-    setDisable({ disabled: 'disabled' });
-    console.log(likes);
+    setLikes(likes + 1)
+    setDisable({ disabled: 'disabled' })
+    console.log(likes)
     db.collection('videos')
       .doc(videoId)
       .update({ likes: likes + 1 })
       .then(() => {
-        console.log('updated');
+        console.log('updated')
       })
-      .catch((err) => {
-        console.log(err);
-      });
-  };
+      .catch(err => {
+        console.log(err)
+      })
+  }
 
-  const getVideo = (id) => {
-    let data;
+  const getVideo = id => {
+    let data
     db.collection('videos')
       .doc(id)
       .get()
-      .then((doc) => {
-        data = doc.data();
-        setLikes(data.likes);
-        setView(data.views);
-        setVideoData(data);
+      .then(doc => {
+        data = doc.data()
+        setLikes(data.likes)
+        setView(data.views)
+        setVideoData(data)
       })
-      .catch((err) => {
-        console.log(err);
-      });
-  };
+      .catch(err => {
+        console.log(err)
+      })
+  }
 
   useEffect(() => {
-    getVideo(videoId);
-  }, []);
+    getVideo(videoId)
+  }, [])
 
   return (
-    <div className="">
+    <div className=''>
       <Navbar />
-      <div className="p-4">
-        <VideoPlayer url={videoData.videoUrl} />
+      <div className='p-4'>
+        <VideoPlayer url={videoData.videoUrl} title={videoData.title} />
       </div>
-      <div className="p-4 ms-4">
+      <div className='p-4 ms-4'>
         <text>{views}</text>
         <h2>{videoData.title}</h2>
         <h6>{videoData.description}</h6>
         <button
-          type="button"
+          type='button'
           onClick={addLike}
-          class="btn btn-outline-danger"
+          class='btn btn-outline-danger'
           {...disabled}
         >
           {likes} {'       '}
           <FavoriteBorderIcon />
         </button>
-        <div className="p-1 mt-4">
+        <div className='p-1 mt-4'>
           <VideoComments videoId={videoId} videoData={videoData} />
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default VideoPage;
+export default VideoPage
