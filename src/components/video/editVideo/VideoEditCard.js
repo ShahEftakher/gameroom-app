2721c2110043d58ae8a6ea30f5cfc06d44f5d270
ShahEftakher <shaheftakher@iut-dhaka.edu>
2021-03-10 12:09:24 +0600

    Comfirm modal implemented

--- src/components/video/editVideo/VideoEditCard.js
+++ src/components/video/editVideo/VideoEditCard.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
-import { Card, Button, Header, Image, Modal, Form } from 'semantic-ui-react';
+import { Card, Button, Header, Icon, Modal, Form } from 'semantic-ui-react';
 import { db, storage } from '../../../firebase';
 import Videoplayer from './ModalVideoplayer';
 
@@ -8,6 +8,7 @@ const VideoEditCard = ({ data, id }) => {
   const [open, setOpen] = useState(false);
   const [title, setTitle] = useState(data.title);
   const [description, setDescription] = useState(data.description);
+  const [confirmModal, setConfirmModal] = useState(false);
   const history = useHistory();
 
   const handleTitleChange = (e) => {
@@ -27,7 +28,9 @@ const VideoEditCard = ({ data, id }) => {
           .doc(id)
           .delete()
           .then(() => {
+            setConfirmModal(false);
             setOpen(false);
+            window.location.reload();
           })
           .catch(() => {});
       })
@@ -63,46 +66,72 @@ const VideoEditCard = ({ data, id }) => {
         }}
       />
       <div className="d-flex justify-content-center">
-        <Modal
-          onClose={() => setOpen(false)}
-          onOpen={() => setOpen(true)}
-          open={open}
-        >
-          <Modal.Content scrolling>
-            <Videoplayer url={data.videoUrl} />
-          </Modal.Content>
-          <Modal.Content>
-            <Form onSubmit={handleEdit}>
-              <Form.Field>
-                <input
-                  type="text"
-                  onChange={handleTitleChange}
-                  defaultValue={data.title}
+        <div>
+          <Modal
+            onClose={() => setOpen(false)}
+            onOpen={() => setOpen(true)}
+            open={open}
+            className="mb-5"
+          >
+            <Modal.Content>
+              <Videoplayer url={data.videoUrl} />
+            </Modal.Content>
+            <Modal.Content scrolling>
+              <Form onSubmit={handleEdit}>
+                <Form.Field>
+                  <input
+                    type="text"
+                    onChange={handleTitleChange}
+                    defaultValue={data.title}
+                  />
+                </Form.Field>
+                <Form.Field>
+                  <input
+                    type="text"
+                    onChange={handleDescriptionChange}
+                    defaultValue={data.description}
+                  />
+                </Form.Field>
+                <Button color="black" positive>
+                  Update
+                </Button>
+              </Form>
+              <Modal.Actions className="d-flex justify-content-end">
+                <Button
+                  content="Delete Video"
+                  labelPosition="right"
+                  color="red"
+                  icon="trash"
+                  onClick={() => {
+                    setConfirmModal(true);
+                  }}
                 />
-              </Form.Field>
-              <Form.Field>
-                <input
-                  type="text"
-                  onChange={handleDescriptionChange}
-                  defaultValue={data.description}
-                />
-              </Form.Field>
-              <Button color="black" positive>
-                Update
-              </Button>
-            </Form>
-          </Modal.Content>
-          <Modal.Actions scrolling>
-            <Button
-              content="Delete Video"
-              labelPosition="right"
-              color="red"
-              icon="trash"
-              onClick={handleDelete}
-            />
-          </Modal.Actions>
-        </Modal>
+              </Modal.Actions>
+            </Modal.Content>
+          </Modal>
+        </div>
       </div>
+      <Modal
+        closeIcon
+        open={confirmModal}
+        onClose={() => setConfirmModal(false)}
+        onOpen={() => setConfirmModal(true)}
+      >
+        <Header icon="trash" content="Delete Video" />
+        <Modal.Content>
+          <p>
+            Do you want to delete this video? It will be deleted permanently!
+          </p>
+        </Modal.Content>
+        <Modal.Actions>
+          <Button color="red" onClick={() => setConfirmModal(false)}>
+            <Icon name="remove" /> No
+          </Button>
+          <Button color="green" onClick={handleDelete}>
+            <Icon name="checkmark" /> Yes
+          </Button>
+        </Modal.Actions>
+      </Modal>
     </div>
   );
 };
