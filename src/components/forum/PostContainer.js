848ba046ed137530df424ea93c56a97f713cb6c7
ESD <ESD@DESKTOP-SNRCVRJ>
2021-03-13 00:37:14 +0600

    Viewer post on profile done

--- src/components/forum/PostContainer.js
+++ src/components/forum/PostContainer.js
@@ -1,41 +1,42 @@
-import React, { useEffect, useState } from 'react'
-import ForumPost from '../../components/forum/ForumPost'
-import { useUserContext } from '../../context/UserContext'
-import { db } from '../../firebase'
+import React, { useEffect, useState } from 'react';
+import ForumPost from '../../components/forum/ForumPost';
+import { useUserContext } from '../../context/UserContext';
+import { db } from '../../firebase';
 
-const PostContainer = () => {
-  const { currentUser } = useUserContext()
-  const [posts, setPosts] = useState([])
+const PostContainer = ({ uid }) => {
+  const { currentUser } = useUserContext();
+  const [posts, setPosts] = useState([]);
 
   const getPost = () => {
-    console.log(posts)
-    console.log('fml')
-    let data = []
-    console.log(data)
+    let data = [];
+    console.log(data);
     db.collection('posts')
       .orderBy('createdAt', 'desc')
-      .onSnapshot(querySnapshot => {
-        querySnapshot.forEach(doc => {
-          data.push({ id: doc.id, post: doc.data() })
-        })
-        setPosts(data)
-        data = []
-      })
-  }
+      .where('uid', '==', uid)
+      .onSnapshot((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+          data.push({ id: doc.id, post: doc.data() });
+        });
+        setPosts(data);
+        data = [];
+      });
+  };
 
   useEffect(() => {
-    getPost()
-  }, [])
+    getPost();
+  }, []);
 
   return (
     <div>
-      <div className='mb-4'>
-        {posts.map(post => {
-          return <ForumPost id={post.id} post={post.post} />
+      <div className="mb-4">
+        <h2>Posts</h2>
+        <hr></hr>
+        {posts.map((post) => {
+          return <ForumPost id={post.id} post={post.post} />;
         })}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default PostContainer
+export default PostContainer;
