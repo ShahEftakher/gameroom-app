4e1a25d82cfda069442a0a68b580182f1700f593
ShahEftakher <shaheftakher@iut-dhaka.edu>
2022-04-24 14:47:42 +0600

    gallery page caching added

--- src/pages/videopages/Gallerypage.js
+++ src/pages/videopages/Gallerypage.js
@@ -5,91 +5,98 @@ import Navbar from '../../components/common/Navbar';
 import VideoFilter from '../../components/video/VideoFilter';
 import { db } from '../../firebase';
 import Footer from '../../components/common/Footer';
+import { useCollection } from 'swr-firestore-v9';
 
 export default function Gallerypage() {
-  const [videos, setVideos] = useState([]);
+  // const [videos, setVideos] = useState([]);
 
-  const getVideos = async () => {
-    let data = [];
-    db.collection('videos')
-      .orderBy('createdAt', 'desc')
-      .onSnapshot((querySnapshot) => {
-        querySnapshot.forEach((doc) => {
-          data.push({
-            id: doc.id,
-            data: doc.data(),
-          });
-        });
-        console.log(data);
-        setVideos(data);
-      });
-  };
+  // const getVideos = async () => {
+  //   let data = [];
+  //   db.collection('videos')
+  //     .orderBy('createdAt', 'desc')
+  //     .onSnapshot((querySnapshot) => {
+  //       querySnapshot.forEach((doc) => {
+  //         data.push({
+  //           id: doc.id,
+  //           data: doc.data(),
+  //         });
+  //       });
+  //       console.log(data);
+  //       setVideos(data);
+  //     });
+  // };
 
-  const getVideosByValorant = () => {
-    let data = [];
-    db.collection('videos')
-      .where('category', '==', 'Valorant')
-      .onSnapshot((querySnapshot) => {
-        querySnapshot.forEach((doc) => {
-          data.push({
-            id: doc.id,
-            data: doc.data(),
-          });
-        });
-        setVideos(data);
-      });
-  };
+  const { data, update, error } = useCollection(`videos`, {
+    orderBy: ['likes', 'desc'],
+    limit: 8,
+    listen: true,
+  });
 
-  const getVideosByR6 = () => {
-    let data = [];
-    db.collection('videos')
-      .where('category', '==', 'R6')
-      .onSnapshot((querySnapshot) => {
-        querySnapshot.forEach((doc) => {
-          data.push({
-            id: doc.id,
-            data: doc.data(),
-          });
-        });
-        setVideos(data);
-      });
-  };
+  // const getVideosByValorant = () => {
+  //   let data = [];
+  //   db.collection('videos')
+  //     .where('category', '==', 'Valorant')
+  //     .onSnapshot((querySnapshot) => {
+  //       querySnapshot.forEach((doc) => {
+  //         data.push({
+  //           id: doc.id,
+  //           data: doc.data(),
+  //         });
+  //       });
+  //       setVideos(data);
+  //     });
+  // };
 
-  const getVideosCSGO = () => {
-    let data = [];
-    db.collection('videos')
-      .where('category', '==', 'CSGO')
-      .onSnapshot((querySnapshot) => {
-        querySnapshot.forEach((doc) => {
-          data.push({
-            id: doc.id,
-            data: doc.data(),
-          });
-        });
-        console.log(data);
-        setVideos(data);
-      });
-  };
+  // const getVideosByR6 = () => {
+  //   let data = [];
+  //   db.collection('videos')
+  //     .where('category', '==', 'R6')
+  //     .onSnapshot((querySnapshot) => {
+  //       querySnapshot.forEach((doc) => {
+  //         data.push({
+  //           id: doc.id,
+  //           data: doc.data(),
+  //         });
+  //       });
+  //       setVideos(data);
+  //     });
+  // };
 
-  const getPopularVideo = () => {
-    let data = [];
-    db.collection('videos')
-      .orderBy('likes', 'desc')
-      .onSnapshot((querySnapshot) => {
-        querySnapshot.forEach((doc) => {
-          data.push({
-            id: doc.id,
-            data: doc.data(),
-          });
-        });
-        console.log(data);
-        setVideos(data);
-      });
-  };
+  // const getVideosCSGO = () => {
+  //   let data = [];
+  //   db.collection('videos')
+  //     .where('category', '==', 'CSGO')
+  //     .onSnapshot((querySnapshot) => {
+  //       querySnapshot.forEach((doc) => {
+  //         data.push({
+  //           id: doc.id,
+  //           data: doc.data(),
+  //         });
+  //       });
+  //       console.log(data);
+  //       setVideos(data);
+  //     });
+  // };
+
+  // const getPopularVideo = () => {
+  //   let data = [];
+  //   db.collection('videos')
+  //     .orderBy('likes', 'desc')
+  //     .onSnapshot((querySnapshot) => {
+  //       querySnapshot.forEach((doc) => {
+  //         data.push({
+  //           id: doc.id,
+  //           data: doc.data(),
+  //         });
+  //       });
+  //       console.log(data);
+  //       setVideos(data);
+  //     });
+  // };
 
   useEffect(() => {
-    setVideos([]);
-    getVideos();
+    // setVideos([]);
+    // getVideos();
   }, []);
 
   return (
@@ -98,18 +105,18 @@ export default function Gallerypage() {
         {/* <Navbar /> */}
         <div className="d-flex justify-content-end me-5">
           <VideoFilter
-            getPopularVideo={getPopularVideo}
-            getNewestVideo={getVideos}
+            // getPopularVideo={getPopularVideo}
+            // getNewestVideo={getVideos}
           />
           <CategoryFilter
-            getVideosByValorant={getVideosByValorant}
-            getVideosByR6={getVideosByR6}
-            getVideosCSGO={getVideosCSGO}
+            // getVideosByValorant={getVideosByValorant}
+            // getVideosByR6={getVideosByR6}
+            // getVideosCSGO={getVideosCSGO}
           />
         </div>
         <div className="mt-3">
-          {videos.map((video) => {
-            return <GalleryVideo id={video.id} video={video.data} />;
+          {data?.map((video) => {
+            return <GalleryVideo id={video.id} video={video} />;
           })}
         </div>
       </div>
